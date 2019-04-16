ea949868e526bd30555814ca9eaf0f5b93155bfe
NazarSemkiv <semkiv.n.i@gmail.com>
2019-04-16 16:27:35 +0300

    basic layout

---
+++ js/fu.js
@@ -0,0 +1,123 @@
+//вперед круізер
+document.getElementById("img-cruiser").onclick = function(){
+  document.getElementById("step-one").style.display = "none";  
+document.getElementById("step-two-cruiser").style.display = "block";  
+};
+
+document.getElementById("btn-two-cruiser").onclick = function(){
+  document.getElementById("step-two-cruiser").style.display = "none";  
+document.getElementById("step-three-cruiser").style.display = "block";  
+};
+
+document.getElementById("btn-three-cruiser").onclick = function(){
+  document.getElementById("step-three-cruiser").style.display = "none";  
+document.getElementById("step-four-cruiser").style.display = "block";  
+};
+
+document.getElementById("btn-four-cruiser").onclick = function(){
+  document.getElementById("step-four-cruiser").style.display = "none";  
+document.getElementById("step-five-cruiser").style.display = "block";  
+};
+
+//назад круізер
+document.getElementById("btn-back-two-cruiser").onclick = function(){
+  document.getElementById("step-one").style.display = "block";  
+document.getElementById("step-two-cruiser").style.display = "none";  
+};
+
+document.getElementById("btn-back-three-cruiser").onclick = function(){
+  document.getElementById("step-two-cruiser").style.display = "block";  
+document.getElementById("step-three-cruiser").style.display = "none";  
+};
+
+document.getElementById("btn-back-four-cruiser").onclick = function(){
+  document.getElementById("step-three-cruiser").style.display = "block";  
+document.getElementById("step-four-cruiser").style.display = "none";  
+};
+
+document.getElementById("btn-back-five-cruiser").onclick = function(){
+  document.getElementById("step-four-cruiser").style.display = "block";  
+document.getElementById("step-five-cruiser").style.display = "none";  
+};
+//вперед лонг
+document.getElementById("img-long").onclick = function(){
+  document.getElementById("step-one").style.display = "none";  
+document.getElementById("step-two-long").style.display = "block";  
+};
+
+document.getElementById("btn-two-long").onclick = function(){
+  document.getElementById("step-two-long").style.display = "none";  
+document.getElementById("step-three-long").style.display = "block";  
+};
+
+document.getElementById("btn-three-long").onclick = function(){
+  document.getElementById("step-three-long").style.display = "none";  
+document.getElementById("step-four-long").style.display = "block";  
+};
+
+document.getElementById("btn-four-long").onclick = function(){
+  document.getElementById("step-four-long").style.display = "none";  
+document.getElementById("step-five-long").style.display = "block";  
+};
+
+//назад лонг
+document.getElementById("btn-back-two-long").onclick = function(){
+  document.getElementById("step-one").style.display = "block";  
+document.getElementById("step-two-long").style.display = "none";  
+};
+
+document.getElementById("btn-back-three-long").onclick = function(){
+  document.getElementById("step-two-long").style.display = "block";  
+document.getElementById("step-three-long").style.display = "none";  
+};
+
+document.getElementById("btn-back-four-long").onclick = function(){
+  document.getElementById("step-three-long").style.display = "block";  
+document.getElementById("step-four-long").style.display = "none";  
+};
+
+document.getElementById("btn-back-five-long").onclick = function(){
+  document.getElementById("step-four-long").style.display = "block";  
+document.getElementById("step-five-long").style.display = "none";  
+};
+//вперед трюковий
+document.getElementById("img-skate").onclick = function(){
+  document.getElementById("step-one").style.display = "none";  
+document.getElementById("step-two-skate").style.display = "block";  
+};
+
+document.getElementById("btn-two-skate").onclick = function(){
+  document.getElementById("step-two-skate").style.display = "none";  
+document.getElementById("step-three-skate").style.display = "block";  
+};
+
+document.getElementById("btn-three-skate").onclick = function(){
+  document.getElementById("step-three-skate").style.display = "none";  
+document.getElementById("step-four-skate").style.display = "block";  
+};
+
+document.getElementById("btn-four-skate").onclick = function(){
+  document.getElementById("step-four-skate").style.display = "none";  
+document.getElementById("step-five-skate").style.display = "block";  
+};
+
+//назад трюковий
+document.getElementById("btn-back-two-skate").onclick = function(){
+  document.getElementById("step-one").style.display = "block";  
+document.getElementById("step-two-skate").style.display = "none";  
+};
+
+document.getElementById("btn-back-three-skate").onclick = function(){
+  document.getElementById("step-two-skate").style.display = "block";  
+document.getElementById("step-three-skate").style.display = "none";  
+};
+
+document.getElementById("btn-back-four-skate").onclick = function(){
+  document.getElementById("step-three-skate").style.display = "block";  
+document.getElementById("step-four-skate").style.display = "none";  
+};
+
+document.getElementById("btn-back-five-skate").onclick = function(){
+  document.getElementById("step-four-skate").style.display = "block";  
+document.getElementById("step-five-skate").style.display = "none";  
+};
